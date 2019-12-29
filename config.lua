1befa433aeb504c403031846fffc81b4aa72f8a3
Stanislav Opletal <stanislav.opletal@gmail.com>
2019-12-29 12:01:57 +0100

    init commit

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+Config.Blips = {
+    {
+        name = "Lanovka",
+        blip = 36,
+        color = 5,
+        type = 4,
+        shortRange = false,
+        scale = 1.5,
+        pos = {
+            x = -760.0,
+            y = 5590.59,
+            z = 36.71
+        }
+    }
+}
